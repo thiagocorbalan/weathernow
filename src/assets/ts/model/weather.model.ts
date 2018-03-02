aed939fc9685a71c61c0f97fff0563215f3657cd
Thiago Corbalan <thiago.co@persona.com.br>
2018-03-02 17:57:35 -0300

    Criação do model e criação do Controller

---
+++ src/assets/ts/model/weather.model.ts
@@ -0,0 +1,7 @@
+export class WeatherModel {
+    private _cityName: string;
+    private _countryName: string;
+    private _temp: number;
+    private _humidity: number;
+    private _pressure: number;
+}
